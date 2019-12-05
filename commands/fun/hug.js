946acefb1cf8f26ced699e8ee55bd33916bb2ad8
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-12-05 20:43:31 +0530

    Create hug.js

---
+++ commands/fun/hug.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const Tenor = require("tenorjs").client({
+	Key: process.env.tenorkey,
+	Filter: "off",
+	Locale: "en_US",
+	MediaFilter: "minimal",
+	DateFormat: "DD/MM/YYYY - H:mm:ss A"
+	});
+
+module.exports = {
+	name: "hug",
+	category: "fun",
+	description: "Hug someone! :3",
+	run: async (client, message, args) => {
+const user = message.mentions.users.first();
+if(!user) return message.reply('please mention someone! Whom you wanna hug?');
+Tenor.Search.Random("anime-hug", "1").then(Results => {
+	Results.forEach(Post => {
+		const img = new Discord.RichEmbed()
+		.setDescription(`**${user.tag}**  Was hugged by  **${message.author.tag}** :3`)
+		.setImage(Post.url)
+		.setColor(0xFFACFA)
+		message.channel.send(img)
+		});
+		})
+		.catch(console.error);
+		}
+	}
