f1c5c4d66e3d0fddfc09d22a202361f8166b0f85
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-12-06 00:49:09 +0530

    Update hug.js

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -9,7 +9,7 @@ module.exports = {
 	run: async (client, message, args) => {
       try {
       const author = message.author.username;
-      const user = message.mentions.users.first();
+      const user = message.mentions.users.first().username;
       if(!user) return message.reply("Please mention a person whom you wanna hug! :3");
       const data = await (await fetch('https://nekos.life/api/v2/img/hug')).json();
       if (!(data || data.url)) return message.reply('NO_DATA');
