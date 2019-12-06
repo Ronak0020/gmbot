9fea442a73886a5e372090533f4faf836edb6faf
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-12-06 15:12:46 +0530

    Create slap.js

---
+++ commands/fun/slap.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = {
+	name: "slap",
+	category: "fun",
+	description: "Slap someone! (>▽<)",
+        usage: "<user mention whom you wanna slap>",
+	run: async (client, message, args) => {
+      try {
+      const author = message.author.username;
+      const user = message.mentions.users.first().username;
+      if(!user) return message.reply("Please mention a person whom you wanna slap! :<");
+      const data = await (await fetch('https://nekos.life/api/v2/img/slap')).json();
+      if (!(data || data.url)) return message.reply('NO_DATA');
+      const slapie = new Discord.RichEmbed()
+          .setTitle(`**${user}** has been slapped by **${author}** (｡•́︿•̀｡)`, true)
+          .setImage(data.url)
+          .setColor(0xFACFCA)
+           message.channel.send(slapie);
+    } catch (error) {
+      console.log(error);
+      return message.reply('Please mention a person whom you wanna slap! :<');
+    }
+		}
+	}
