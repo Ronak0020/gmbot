777ca3859e5b6b54da384ea5d622b1c64e5d9954
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-12-09 16:37:36 +0530

    economy commands

--- index.js
+++ index.js
@@ -80,4 +80,4 @@ client.on("message", async message => {
         command.run(client, message, args);
 });
 
-client.login("NjM4NjMzNzQwODQ2NzU5OTM2.Xe4cLw.pv4ST7UDuJSf71ovZvGYGvfK0BM");
+client.login(process.env.token);
