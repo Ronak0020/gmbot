fda12f69fbb3c33bc9ca8a47c261620f52f66cc0
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-01-05 22:10:01 +0530

    Update rank.js

--- commands/levels/rank.js
+++ commands/levels/rank.js
@@ -14,11 +14,10 @@ const user = await Levels.fetch(target.id, message.guild.id, 1000); // Selects t
 if (!user) return message.channel.send("Seems like this user has not earned any xp so far."); // If there isnt such user in the database, we send a message in general.
 const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id);
 const leader = Levels.computeLeaderboard(client, rawLeaderboard);
-const position = leader(user => `${user.position}`);
  
 const embed = new Discord.RichEmbed()
 .setTitle("Rank", target.displayAvatarURL)
-.setDescription(`${target.username}\'s Rank :\n**Level -** ${user.level}\n**XP -** ${user.xp}\n**Position in Leaderboard -** ${position}`)
+.setDescription(`${target.username}\'s Rank :\n**Level -** ${user.level}\n**XP -** ${user.xp}\n**Position in Leaderboard -** ${leader.user.position}`)
 .setColor('RANDOM')
 .setThumbnail(target.displayAvatarURL)
 .setFooter(client.user.username, client.user.displayAvatarURL)
