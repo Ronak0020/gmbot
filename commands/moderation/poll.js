8453d430636290d1b784caa36e9a7b791a25f0b2
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-01-14 15:06:38 +0530

    Update poll.js

--- commands/moderation/poll.js
+++ commands/moderation/poll.js
@@ -6,16 +6,15 @@ module.exports = {
     description: "Start a poll to get your server member's views!",
     usage: "<Poll info>",
     run: async(client, message, args) => {
-       if (!message.guild.member(client.user).hasPermission('ADD_REACTIONS')) return message.reply('Sorry, i dont have the perms to do this cmd i need ADD_REACTIONS. :x:')
-      const sayMessage = args.slice(1).join(" ");
-      const channels = message.guild.mentions.channels.first();
+      if (!message.guild.member(client.user).hasPermission('ADD_REACTIONS')) return message.reply('Sorry, i dont have the perms to do this cmd i need ADD_REACTIONS. :x:')
+      const sayMessage = args.join(" ");
       if (sayMessage.length < 1) return message.channel.send("Didnt provide anything for the poll.")
       if (message.member.hasPermission("ADMINISTRATOR")) {
        const embed = new Discord.RichEmbed()
        .setColor(0x00A2E8)
        .setTitle(" Poll ")
        .setDescription(`A poll has begun: \n"***${sayMessage}***"\n **VOTE NOW!**`)
-        channels.send(embed).then(m => {
+        message.channel.send(embed).then(m => {
             m.react('✅');
             m.react('❌');
            })
