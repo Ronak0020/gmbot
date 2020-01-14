6fd2adcb1d7d58b4719ec6fed0af4f6d7ae9458a
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-01-14 14:55:32 +0530

    Update poll.js

--- commands/moderation/poll.js
+++ commands/moderation/poll.js
@@ -6,12 +6,11 @@ module.exports = {
     description: "Start a poll to get your server member's views!",
     usage: "<#channel for poll> <Poll info>",
     run: async(client, message, args) => {
-      if(!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("You do not have permission to start a poll!");
         if (!message.guild.member(client.user).hasPermission('ADD_REACTIONS')) return message.reply('Sorry, i dont have the perms to do this cmd i need ADD_REACTIONS. :x:')
       const sayMessage = args.slice(1).join(" ");
       const channel = message.guild.mentions.channels.first();
      if (sayMessage.length < 1) return message.channel.send("Didnt provide anything for the poll.")
-     if (message.member.hasPermission("KICK_MEMBERS")) {
+     if (message.member.hasPermission("ADMINISTRATOR")) {
        const embed = new Discord.RichEmbed()
        .setColor(0x00A2E8)
        .setTitle(" Poll ")
